21b2efc8eb9b7f6f5d1f739cfd022034f90f7a98
Venkat1100 <sailoveds@gmail.com>
2019-06-16 00:01:51 +0530

    New Commit
    New commit

---
+++ src/test/java/SeleniumProject/Practice/NewTest.java
@@ -0,0 +1,29 @@
+package SeleniumProject.Practice;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.AfterTest;
+
+@SuppressWarnings("unused")
+public class NewTest {
+	private WebDriver driver;
+
+	@Test
+	public void testEasy() {
+		
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		
+	}
+
+	@AfterTest
+	public void afterTest() {
+		
+	}
+}
